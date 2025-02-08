c153a15613de0abbd084340fc61cbd953422c10f
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-07 21:19:43 -0300

    feat: Create database

---
+++ src/data/dataCard.ts
@@ -0,0 +1,25 @@
+import { Publications } from "../interfaces/IPublications";
+
+export const dataCard: Array<Publications> = [
+  {
+    id: 1,
+    title: "O que é linguagem de programação? Conheça as principais",
+    description: `Uma das mais populares vertentes da tecnologia da informação, a
+            área de programação segue tendo muita demanda de trabalho
+            justamente pela velocidade com que dispositivos tecnológicos vêm
+            avançando.`,
+    date: "17 de ago, 2024"
+  },
+  {
+    id: 2,
+    title: "GitHub agora permite fazer login sem precisar de senha",
+    description: ` O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.`,
+    date: "12 de jul, 2024"
+  },
+  {
+    id: 3,
+    title: "Por que os hiperlinks são azuis em sua maioria?",
+    description: `Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks – uma palavra ou termo clicável que direciona o leitor.`,
+    date: "21 de jul, 2024"
+  },
+];
