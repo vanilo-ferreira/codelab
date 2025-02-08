93353677258f6747a697b98a4535b62650c16dee
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-07 22:03:19 -0300

    feat: Add function to like posts

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,8 @@
 import "./style.css";
 import heart from "../../assets/heart.svg";
-import litheart from "./assets/litheart.svg";
+import litheart from "../../assets/litheart.svg";
+
+import { useState } from "react";
 
 type Props = {
   title: string;
@@ -8,19 +10,26 @@ type Props = {
   date: string;
 };
 
-export const Card = ({title, description, date}: Props) => {
+export const Card = ({ title, description, date }: Props) => {
+  const [liked, setLiked] = useState(false);
+
+  function like() {
+    setLiked(!liked)
+  }
+
   return (
     <div className="card">
       <div className="cardTopo">
         <p className="cardDate">{date}</p>
-        <img className="cardIcon" src={heart} alt="Heart" />
+        <img
+          className="cardIcon"
+          onClick={() => like()}
+          src={liked === false ? heart : litheart}
+          alt="Heart"
+        />
       </div>
-      <h2 className="cardTitle">
-        {title}
-      </h2>
-      <p className="cardText">
-        {description}
-      </p>
+      <h2 className="cardTitle">{title}</h2>
+      <p className="cardText">{description}</p>
     </div>
   );
 };
