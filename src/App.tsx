6fc0e28e17eff49684b3aa63de08de50be90c042
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-07 21:22:01 -0300

    feat: Create cards on the screen based on the dataCard database

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import "./App.css";
 
 import codelab from "./assets/codelab.svg";
-import litheart from "./assets/litheart.svg";
-import heart from "./assets/heart.svg";
-import { Card } from './components/Card';
+import { Card } from "./components/Card";
+
+import { dataCard } from "./data/dataCard";
 
 function App() {
   return (
@@ -19,43 +19,13 @@ function App() {
         />
       </header>
       <main>
-        <div className="card">
-          <div className="cardTopo">
-            <p className="cardDate">17 de ago, 2024</p>
-            <img className="cardIcon" src={litheart} alt="Lit Heart" />
-          </div>
-          <h2 className="cardTitle">O que é linguagem de programação? Conheça as principais</h2>
-          <p className="cardText">
-            Uma das mais populares vertentes da tecnologia da informação, a
-            área de programação segue tendo muita demanda de trabalho
-            justamente pela velocidade com que dispositivos tecnológicos vêm
-            avançando.
-          </p>
-        </div>
-
-        <div className="card">
-          <div className="cardTopo">
-            <p className="cardDate">12 de jul, 2024</p>
-            <img className="cardIcon" src={heart} alt="Heart" />
-          </div>
-          <h2 className="cardTitle">GitHub agora permite fazer login sem precisar de senha</h2>
-          <p className="cardText">
-            O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.
-          </p>
-        </div>
-
-        <Card />
-
-        {/* <div className="card">
-          <div className="cardTopo">
-            <p className="cardDate">21 de jul, 2024</p>
-            <img className="cardIcon" src={heart} alt="Heart" />
-          </div>
-          <h2 className="cardTitle">Por que os hiperlinks são azuis em sua maioria?</h2>
-          <p className="cardText">
-            Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks – uma palavra ou termo clicável que direciona o leitor.
-          </p>
-        </div> */}
+        {dataCard.map((data) => (
+          <Card
+            title={data.title}
+            description={data.description}
+            date={data.date}
+          />
+        ))}
       </main>
     </>
   );
